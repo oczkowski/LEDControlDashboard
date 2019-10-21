2d413b76ecf44e0d652ab38f446906d9928ab6fa
Kamil Oczkowski <kamil@oczkow.ski>
2019-10-21 20:11:37 +0100

    Base dashboard layout

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,5 +1,43 @@
 import React from 'react';
+// Material UI
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex'
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column'
+    },
+    fixedHeight: {
+        height: 300
+    }
+}));
 
 export default props => {
-    return <div>Dashboard</div>;
+    const classes = useStyles();
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+    return (
+        <Grid container spacing={3}>
+            {/* Chart */}
+            <Grid item xs={12} md={7} lg={8}>
+                <Paper className={fixedHeightPaper}>Test</Paper>
+            </Grid>
+            {/* Recent Deposits */}
+            <Grid item xs={12} md={5} lg={4}>
+                <Paper className={fixedHeightPaper}>Test</Paper>
+            </Grid>
+            {/* Recent Orders */}
+            <Grid item xs={12}>
+                <Paper className={classes.paper}>TEST</Paper>
+            </Grid>
+        </Grid>
+    );
 };
