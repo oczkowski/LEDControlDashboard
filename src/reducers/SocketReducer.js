ae3e431c7bff99dc267a3a7ecb6f561235c78dbf
Kamil Oczkowski <kamil@oczkow.ski>
2019-11-10 11:43:27 +0000

    Now combining objects on new rooms receive and keep alive udpdate

--- src/reducers/SocketReducer.js
+++ src/reducers/SocketReducer.js
@@ -1,17 +1,20 @@
 // Action types
 import { S_RECEIVE_ROOMS, S_UPDATE_ONLINE_STATUS } from '../actions/types';
 
+function combineObjectPropertyObject(state, objectToMerge) {
+    let combined = {};
+    for (let key in objectToMerge) {
+        combined[key] = { ...state[key], ...objectToMerge[key] };
+    }
+    return { ...state, ...combined };
+}
+
 export default (state = {}, action) => {
     switch (action.type) {
         case S_RECEIVE_ROOMS:
-            return { ...state, ...action.payload.rooms };
+            return combineObjectPropertyObject(state, action.payload.rooms);
         case S_UPDATE_ONLINE_STATUS:
-            var updatedRooms = {};
-            let { status } = action.payload;
-            for (let key in status) {
-                updatedRooms[key] = { ...state[key], status: status[key] };
-            }
-            return { ...state, ...updatedRooms };
+            return combineObjectPropertyObject(state, action.payload.status);
         default:
             return state;
     }
