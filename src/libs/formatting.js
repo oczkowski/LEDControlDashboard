6bd9287195e12b1cd5ca64de1184daa8290476f3
Kamil Oczkowski <kamil@oczkow.ski>
2019-11-23 16:49:27 +0000

    New formatting internal lib

---
+++ src/libs/formatting.js
@@ -0,0 +1,37 @@
+// Importing react
+import React from 'react';
+// Styling
+import './formatting.css';
+
+// Functions
+export const getStatus = (status, isNew = false) => {
+    let statusString = capitalize(status);
+    let style = {
+        padding: '2px',
+        width: '60px',
+        textAlign: 'center',
+        backgroundColor: '#db2f00',
+        color: 'white',
+        borderRadius: '10px',
+        fontWeight: '450',
+        fontFamily: "'Inconsolata', monospace",
+        userSelect: 'none'
+    };
+
+    if (status === 'online') style.backgroundColor = '#41b300';
+    if (status === 'reboot') style.backgroundColor = '#0278cc';
+    if (isNew) {
+        style.cursor = 'pointer';
+        style.width = '95px';
+        statusString += ' (NEW)';
+    }
+
+    return (
+        <div style={style} alt={status}>
+            {statusString}
+        </div>
+    );
+};
+
+export const capitalize = string =>
+    string.charAt(0).toUpperCase() + string.slice(1);
